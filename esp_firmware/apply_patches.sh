73212a5d084bc6fccdb2bd30764c5238d0553ef5
Daniel Brill <179643835+DanielBrill20@users.noreply.github.com>
2025-11-21 00:19:49 -0500

    Copilot suggestions to improve patch script

--- esp_firmware/apply_patches.sh
+++ esp_firmware/apply_patches.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+cd "$(dirname "$0")"
+set -e
 git submodule update --init --recursive
 git apply patches/Adafruit_BusIO.patch --directory=components/Adafruit_BusIO
 git apply patches/Adafruit-GFX-Library.patch --directory=components/Adafruit-GFX-Library
