93f8067b732273abf169e4f994101378795f35d3
Daniel Brill <179643835+DanielBrill20@users.noreply.github.com>
2025-11-26 00:46:13 -0500

    Ensure C linkage for wifi and server components

--- esp_firmware/main/wifi_manager.h
+++ esp_firmware/main/wifi_manager.h
@@ -1,6 +1,10 @@
 #ifndef WIFI_MANAGER_H
 #define WIFI_MANAGER_H
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 #include "esp_err.h"
 #include "esp_log.h"
 #include "freertos/FreeRTOS.h"
@@ -8,6 +12,7 @@
 #include "esp_wifi.h"
 #include "nvs_flash.h"
 #include "esp_netif.h"
+#include "mdns.h"
 
 /**
  * @brief   Connects the ESP as a WiFi station to an access point specified by menuconfig.
@@ -16,8 +21,12 @@
  *          with the provided SSID, password, and minimum security threshold of WIFI_AUTH_WPA2_PSK,
  *          and finally connecting the station to the access point.
  *          It also utilizes FreeRTOS wait bits to halt program execution until connection is finished
- *          and an IP address is obtained.
+ *          and an IP address is obtained. Once finished, it establishes an mDNS hostname specified by menuconfig.
  */
 void wifi_manager_init();
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif // WIFI_MANAGER_H
