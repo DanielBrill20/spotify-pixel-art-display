8f553b1a0fa86e3ea9b4517037be65b5ee3706db
Daniel Brill <179643835+DanielBrill20@users.noreply.github.com>
2025-11-28 21:49:51 -0500

    Extern C for linkage

--- esp_firmware/main/matrix_driver.h
+++ esp_firmware/main/matrix_driver.h
@@ -1,10 +1,18 @@
 #ifndef MATRIX_DRIVER_H
 #define MATRIX_DRIVER_H
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 #include "esp_err.h"
 
 esp_err_t display_image();
 esp_err_t display_screensaver();
 esp_err_t matrix_driver_init();
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif // MATRIX_DRIVER_H
