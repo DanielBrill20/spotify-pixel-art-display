b867e240ecb55d1ef4ed394cc5d2c00bca3781a4
Daniel Brill <179643835+DanielBrill20@users.noreply.github.com>
2025-11-29 00:54:31 -0500

    Function docstrings

--- esp_firmware/main/matrix_driver.h
+++ esp_firmware/main/matrix_driver.h
@@ -7,8 +7,31 @@ extern "C" {
 
 #include "esp_err.h"
 
+/**
+ * @brief   Displays an image on the matrix as stored in `image_buf` from `http_server.h`.
+ * 
+ * @returns `ESP_OK` if image display is successful, otherwise `ESP_FAIL` if matrix is uninitialized.
+ */
 esp_err_t display_image();
+
+/**
+ * @brief   Clears the matrix. (temporary, will implement screensaver later)
+ * 
+ * @returns `ESP_OK` if screensaver mode succeeds, otherwise `ESP_FAIL` if matrix is uninitialized.
+ */
 esp_err_t display_screensaver();
+
+/**
+ * @brief   Initializes the HUB75 LED Matrix with a custom configuration for
+ *          width, height, chained panels, and pinout as defined in `idf.py menuconfig`.
+ *          Sets clock phase to false for panels with a "negative clock edge".
+ *          Enables double buffer for smoother image transitions.
+ *          Starts matrix with a clear screen and proper brightness.
+ * 
+ * @returns `ESP_OK` if panel intialization is successful,
+ *          `ESP_ERR_NO_MEM` if `MatrixPanel_I2S_DMA` fails,
+ *          otherwise `ESP_FAIL`.
+ */
 esp_err_t matrix_driver_init();
 
 #ifdef __cplusplus
