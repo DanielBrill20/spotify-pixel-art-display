1f974acdfd5c4dd9b9e843f70236efabfc1790f3
Daniel Brill <179643835+DanielBrill20@users.noreply.github.com>
2025-12-28 02:28:24 -0500

    Quick git patches fix

--- esp_firmware/apply_patches.cmd
+++ esp_firmware/apply_patches.cmd
@@ -3,9 +3,9 @@ setlocal
 cd /d "%~dp0"
 
 git submodule update --init --recursive || goto :error
-git apply patches/Adafruit_BusIO.patch --directory=components/Adafruit_BusIO || goto :error
-git apply patches/Adafruit-GFX-Library.patch --directory=components/Adafruit-GFX-Library || goto :error
-git apply patches/ESP32-HUB75-MatrixPanel-DMA.patch --directory=components/ESP32-HUB75-MatrixPanel-DMA || goto :error
+git apply patches/Adafruit_BusIO.patch --directory=esp_firmware/components/Adafruit_BusIO || goto :error
+git apply patches/Adafruit-GFX-Library.patch --directory=esp_firmware/components/Adafruit-GFX-Library || goto :error
+git apply patches/ESP32-HUB75-MatrixPanel-DMA.patch --directory=esp_firmware/components/ESP32-HUB75-MatrixPanel-DMA || goto :error
 
 echo Done.
 exit /b 0
