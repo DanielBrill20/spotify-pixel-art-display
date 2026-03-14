2c2ec11d5d8a69247275f6b7293b5eddb6f862e3
Daniel Brill <179643835+DanielBrill20@users.noreply.github.com>
2026-03-14 18:38:55 -0400

    Add time_manager module

---
+++ esp_firmware/main/time_manager.h
@@ -0,0 +1,24 @@
+#ifndef TIME_MANAGER_H
+#define TIME_MANAGER_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include "esp_err.h"
+
+/**
+ * @brief   Initializes and synchronizes the system clock.
+ *          Utilizes the Simple Network Time Protocol (SNTP) to get UTC time.
+ *          Configures the timezone to US Eastern Time (EST/EDT).
+ * 
+ * @returns `ESP_OK` if configuration is successful,
+ *          `ESP_FAIL` if the SNTP sync is not sucessful within 10 seconds.
+ */
+esp_err_t time_manager_init();
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // TIME_MANAGER_H
