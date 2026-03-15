cee1d76b3c38e1656c244937cd4ce42de16739c0
Daniel Brill <179643835+DanielBrill20@users.noreply.github.com>
2026-03-14 20:53:12 -0400

    Updates to main and CMake for the new modules

--- esp_firmware/main/main.cpp
+++ esp_firmware/main/main.cpp
@@ -1,10 +1,14 @@
 #include "wifi_manager.h"
 #include "http_server.h"
 #include "matrix_driver.h"
+#include "time_manager.h"
+#include "analog_clock.h"
 
 extern "C" void app_main(void)
 {
     ESP_ERROR_CHECK(matrix_driver_init());
     ESP_ERROR_CHECK(wifi_manager_init());
     ESP_ERROR_CHECK(http_server_init());
+    ESP_ERROR_CHECK(time_manager_init());
+    analog_clock_init();
 }
